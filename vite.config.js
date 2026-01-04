e683b4c82da60659607e9bc2dfb659a700e77ad7
MohammedArmaaan <193485809+MohammedArmaaan@users.noreply.github.com>
2026-01-04 19:01:46 +0530

    Portfolio - Update 4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Niche wali line add karein. 
+  // 'REPO_NAME' ki jagah apne GitHub Repository ka naam likhein.
+  // Example: Agar repo ka naam 'portfolio' hai to base: '/portfolio/'
+  base: "/REPO_NAME/", 
 })
